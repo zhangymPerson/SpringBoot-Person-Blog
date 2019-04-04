29a73366119c8b678ecb8df2039729eaa6bd33fc
cooix <zhangyanmingjiayou@163.com>
2019-04-04 11:51:15 +0800

    project create-project

---
+++ src/main/java/com/danao/PersonBlogApplication.java
@@ -0,0 +1,22 @@
+package com.danao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhang
+ * @version 1.0
+ * @classname PersonBlogApplication
+ * @descriptionclass class describe
+ * 1.****************
+ * 2.****************
+ * @createdate 2019/4/4
+ * @since 1.0
+ */
+@SpringBootApplication
+public class PersonBlogApplication {
+	public static void main(String[] args) {
+		//启动类
+		SpringApplication.run(PersonBlogApplication.class);
+	}
+}
---
+++ src/main/java/com/danao/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.danao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhang
+ * @version 1.0
+ * @classname HelloController
+ * @descriptionclass class describe
+ * 1.测试
+ * @createdate 2019/4/4
+ * @since 1.0
+ */
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+	@RequestMapping(value = "/test")
+	public String test(){
+		return "success 萨达阿瑟东sad; ";
+	}
+}
